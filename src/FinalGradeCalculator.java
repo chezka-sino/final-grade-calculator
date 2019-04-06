32d1918bccf7018e04c6e813429cc2cc3b32fc16
chezka-sino <chezka@pm.me>
2019-04-06 12:51:08 -0700

    Create Student class

---
+++ src/FinalGradeCalculator.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FinalGradeCalculator {
+
+	public static void main(String[] args) {
+		 
+		// Will contain list of Student objects from the input file
+		ArrayList<Student> roster = new ArrayList<>();
+		
+		
+		
+		/**
+		 * TODO file reader
+		 * read comma delimited txt file with header
+		 */
+		
+		/**
+		 * TODO storing student data
+		 * ArrayList of Student objects
+		 * 
+		 */
+		
+		/**
+		 * TODO Student class
+		 * constructor to automatically convert str grades to int
+		 * 
+		 */
+		
+		/**
+		 * TODO GUI
+		 * displays final grade for selected student
+		 */
+		
+		/**
+		 * waiting for clarifications from professor
+		 * *output
+		 * *letter grade tally?
+		 * 
+		 */
+	}
+
+}
---
+++ src/Student.java
@@ -0,0 +1,111 @@
+
+public class Student {
+	
+	private String name;
+	private int hw1;
+	private int hw2;
+	private int hw3;
+	private int midterm;
+	private int project;
+	private int finals;
+	private double finalGrade;
+	private char letterGrade;
+	
+	public Student(String name, int hw1, int hw2, int hw3, int midterm, int project, int finals) {
+		this.name = name;
+		this.hw1 = hw1;
+		this.hw2 = hw2;
+		this.hw3 = hw3;
+		this.midterm = midterm;
+		this.project = project;
+		this.finals = finals;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHw1() {
+		return hw1;
+	}
+
+	public void setHw1(int hw1) {
+		this.hw1 = hw1;
+	}
+
+	public int getHw2() {
+		return hw2;
+	}
+
+	public void setHw2(int hw2) {
+		this.hw2 = hw2;
+	}
+
+	public int getHw3() {
+		return hw3;
+	}
+
+	public void setHw3(int hw3) {
+		this.hw3 = hw3;
+	}
+
+	public int getMidterm() {
+		return midterm;
+	}
+
+	public void setMidterm(int midterm) {
+		this.midterm = midterm;
+	}
+
+	public int getProject() {
+		return project;
+	}
+
+	public void setProject(int project) {
+		this.project = project;
+	}
+
+	public int getFinals() {
+		return finals;
+	}
+
+	public void setFinals(int finals) {
+		this.finals = finals;
+	}
+	
+	public void calculateFinalGrade() {
+		this.finalGrade = (0.45 * ((hw1+hw2+hw3)/3)) + 
+				(0.25 * project) + 
+				(0.3 * ((midterm + finals)/2));
+	}
+	
+	public double getFinalGrade() {
+		return finalGrade;
+	}
+	
+	public char getLetterGrade() {
+		if (finalGrade <= 100 & finalGrade >= 90) {
+			this.letterGrade = 'A';
+		}
+		else if (finalGrade < 90 & finalGrade >= 80) {
+			this.letterGrade = 'B';
+		}
+		else if (finalGrade < 80 & finalGrade >= 70) {
+			this.letterGrade = 'C';
+		}
+		else if (finalGrade < 70 & finalGrade >= 60) {
+			this.letterGrade = 'D';
+		}
+		else {
+			this.letterGrade = 'F';
+		}
+		
+		return letterGrade;
+			
+	}
+
+}
